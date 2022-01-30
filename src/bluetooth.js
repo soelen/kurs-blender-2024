02d5e4bc5ca2e39658fc316ba666a9757ce5f24e
Uwe Sölen Flaig <uwe.soelen.flaig@gmail.com>
2022-01-30 21:11:00 +0100

    Updated icons, added disconnected feature and updated wc.

--- src/bluetooth.js
+++ src/bluetooth.js
@@ -1,48 +1,44 @@
 let characteristic;
+let bluetoothDevice;
 
 const connect = ( filter, service ) => {
-  return new Promise(function( resolve, reject) {
-    console.log('yes')
-    console.log( filter, service );
+  return new Promise( function( resolve, reject) {
 
     return navigator.bluetooth.requestDevice( {
         acceptAllDevices: true,
-        optionalServices: [
-            // 'battery_service',
-            // parseInt( service ),
-            0x18F0,
-        ]
+        optionalServices: [ 0x18F0, ]
     })
     .then( ( device ) => {
-        console.log('gatt', device );
+        bluetoothDevice = device;
 
         const gatt = device.gatt;
 
         return gatt.connect();
     } )
     .then( server => {
-        console.log('get primary service');
-        // return server.getPrimaryServices();
-        // console.log( server.connected );
         return server.getPrimaryService( 0x18F0 );
     } )
     .then( ( service ) => {
-        // console.log('services', services );
-
-        // const service = services.find( service => service.isPrimary );
         return service.getCharacteristics();
     } )
     .then( ( characteristics ) => {
-        console.log('characteristics');
         characteristic = characteristics.find( characteristic => characteristic.properties.write );
         resolve( characteristic );
         return characteristic;
     }).catch( error => {
         reject( error );
-        console.log( error.message );
     });
   });
 
+}
+
+const disconnect = () => {
+  if (!bluetoothDevice) return false;
+  if (bluetoothDevice.gatt.connected) {
+    bluetoothDevice.gatt.disconnect();
+    return true;
+  }
+  return false;
 }
 
 const print = ( data ) => {
@@ -53,31 +49,17 @@ const print = ( data ) => {
       array.push( data[i].charCodeAt(0) );
     }
 
-
-    // array = [ 0x01b, 82, 50, ...array, ...[0x01B, 0x64, 2 ] ];
     const mapped = textEncoder( data );
-    console.log( mapped );
     Promise.all( mapped ).then( data => {
-        const reducedMap = mapped;
-    //   const reducedMap = data.reduce( ( accumulator, map ) => [ ...accumulator, ...map.escpos ], [] );
-      // array = [ 0x01b, 82, 50, ...reducedMap, ...[0x01B, 0x64, 2 ] ];
-      // var chunk_size = 110;
       var chunk_size = 110;
-      // var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
       var groups = []
-      // groups.push( [0x01B, 0x64, 2 ] )
-      groups = reducedMap.map( ( character, index ) => {
-
-        return index % chunk_size === 0 ? reducedMap.slice( index, index + chunk_size ) : null;
-
+      groups = mapped.map( ( character, index ) => {
+        return index % chunk_size === 0 ? mapped.slice( index, index + chunk_size ) : null;
       } ).filter( character => character );
 
-      groups.push( [0x01B, 0x64, 2 ] )
-      recursivefunction( groups );
-      });
-    // characteristic.writeValue( new Uint8Array( array ).buffer );
-
-
+      groups.push( [ 0x01B, 0x64, 2 ] )
+      chunkStreamer( groups );
+    });
 
 }
 
@@ -91,12 +73,12 @@ const textEncoder = ( string ) => {
 
 }
 
-const recursivefunction = ( groups, index = 0 ) => {
+const chunkStreamer = ( groups, index = 0 ) => {
   if( !characteristic ) return;
 
   characteristic.writeValue( new Uint8Array( groups[ index ] ).buffer ).then( () => {
-      if( groups[ index + 1 ] ) recursivefunction( groups, index + 1 );
+      if( groups[ index + 1 ] ) chunkStreamer( groups, index + 1 );
   });
 }
 
-export { connect, print, }
+export { connect, print, disconnect }
--- src/components/wm-bluetooth.js
+++ src/components/wm-bluetooth.js
@@ -1,22 +1,11 @@
-import { html, LitElement } from 'lit';
-// const template = document.createElement('template');
-// template.innerHTML = `
-// <div>
-//     <!--Healine-->
-//     <p>Rating</p>
-//     <!--rating-stars-->
-// </div>
-// `;
-// 
-import { connect, print, } from '../bluetooth';
+import { html, css, LitElement, } from 'lit';
+import { connect, print, disconnect, } from '../bluetooth';
 
 class WmBluetooth extends LitElement {
 
-    // _isConnected = false;
-    // _text = '';
-
     static properties = {
         _isConnected: {state: true, },
+        _isConnecting: { state: true, },
         _text: { state: true, },
     };
 
@@ -24,29 +13,111 @@ class WmBluetooth extends LitElement {
         super();
     }
 
+    static get styles() { return css`
+    *, ::after, ::before {
+        box-sizing: border-box;
+    }
+    :host {
+        display: block;
+    }
+        form {
+        }
+        #textarea-wrapper {
+            padding: 16px;
+            background: white;
+            border-radius: 8px;
+        }
+        textarea {
+            border: none;
+            min-height: 200px;
+        }
+        textarea, button {
+            text-align: left;
+            margin-left: auto;
+            margin-right: auto;
+            resize: vertical;
+            color: black;
+            display: block;
+            width: 100%;
+            white-space: nowrap;
+        }
+        button[disabled] {
+            opacity: .5;
+        }
+        button {
+            all:unset;
+            cursor: pointer;
+            border-radius: 8px;
+            text-transform: uppercase;
+            background: white;
+            padding: 8px 16px;
+            font-size: 16px;
+            color: black;
+        }
+
+        img {
+            width: 16px;
+        }
+
+        button[hidden] {
+            display: none;
+        }
+
+        #buttons {
+            margin-top: 16px;
+            display: flex;
+            gap: 16px;
+        }
+
+        #buttons button:first-child {
+            flex-grow: 1;
+        }
+    `;
+
+    }
+
     render() {
         return html`
-        <form @submit="${ ( event ) => {
-            event.preventDefault();
-            console.log('hello')
-        }}">
-            <textarea @input="${ event => {
-              this._text = event.currentTarget.value;
-            }}"></textarea>
-            <div>
-                <button type="button" @click="${ event => {
-                    console.log('yesll');
-                    connect().then( characteristic => {
-                        this._isConnected = true;
-                        console.log( 'mission successful');
-                    } );
-                }}">Connect</button>
-                <button ?disabled="${ !this._isConnected }" @click="${ event => {
-                    print( this._text );
-                    console.log('bb')
-                }}">Send</button>
-            </div>
-        </form>
+        <div style="border: 1px soldid pink;">
+            <form @submit="${ ( event ) => {
+                event.preventDefault();
+                console.log('hello')
+            }}">
+                <div id="textarea-wrapper">
+                    <textarea @input="${ event => {
+                    this._text = event.currentTarget.value;
+                    }}"></textarea>
+                </div>
+                <div id="buttons">
+                    <button
+                        ?hidden="${ !this._isConnected }"
+                        type="button"
+                        @click="${ event => {
+                        disconnect();
+                        this._isConnected = false;
+                    }}">Trennen</button>
+                    <button
+                        ?hidden="${ this._isConnected }"
+                        ?disabled="${ this._isConnecting }"
+                        type="button"
+                        @click="${ event => {
+                            this._isConnecting = true;
+                        connect().then( characteristic => {
+                            this._isConnected = true;
+                            this._isConnecting = false;
+                        } ).catch( () => this._isConnecting = false ); }}"
+                    >
+                            <img src="/assets/icons/connect.svg" >
+                        Verbinden</button>
+                    <button
+                        ?disabled="${ !this._isConnected }"
+                        @click="${ event => print( this._text )}"
+                    >
+                            <img src="/assets/icons/send.svg" >
+                    Senden</button>
+                </div>
+            </form>
+        </div>
         `;
     }
     
